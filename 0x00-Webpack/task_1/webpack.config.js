5473e45674e07c9a52d3bb3f609c7fb3c775c3c9
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-06-27 22:18:03 +0300

    location change for js folder

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
