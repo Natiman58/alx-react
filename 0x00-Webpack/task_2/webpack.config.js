a47d0d5b459aea3f416013bedcec61b36ebe3078
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-06-30 13:42:32 +0300

    add css and image

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: "bundle.js",
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)$/,
+            type: 'asset/resource',
+          },
+        ],
+      },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+};
