f1685c59d56e1587d9182c99ecea52d8e672aae7
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-07-03 17:45:51 +0300

    basic tests for getFullYear getFooterCopy and getLatestNotification function

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,16 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+
+test('getFullYear', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toEqual(currentYear);
+});
+
+test('getFooterCopy', () => {
+    expect(getFooterCopy("True")).toEqual("Holberton School");
+    expect(getFooterCopy("False")).toEqual("Holberton School main dashboard");
+});
+
+test('getLatestNotification', () => {
+    expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
+});
