01ab58c0c0521d04fb2400ca6cd0e6d5c1fa1610
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-07-10 13:13:04 +0300

    components

--- 0x02-react_props/dashboard/src/Login/Login.js
+++ 0x02-react_props/dashboard/src/Login/Login.js
@@ -4,18 +4,19 @@ import './Login.css';
 
 function Login() {
     return (
-        <React.Fragment>
-             <div className='App-body'>
-        <p>Login to access the full dashboard</p>
-        <form>
-        <label htmlFor='email'> Email: <input type='text' name='email'/> </label>
-        <label htmlFor='password'> Password: <input type='text' name='password'/> </label>
-        <button className='Ok-button' type='submit'>OK</button>
-        </form>
-      </div>
+    <React.Fragment>
 
-      </React.Fragment>
-    );
+        <div className='App-body'>
+            <p>Login to access the full dashboard</p>
+            <form>
+                <label htmlFor='email'> Email: <input type='text' name='email'/> </label>
+                <label htmlFor='password'> Password: <input type='text' name='password'/> </label>
+                <button className='Ok-button' type='submit'>OK</button>
+            </form>
+        </div>
+
+    </React.Fragment>
+);
 }
 
 export default Login;
